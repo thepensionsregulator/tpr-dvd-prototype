c3c8f448572e0c29e4050298867ca75dd1bef0df
mrkwrght <markwright785@gmail.com>
2023-01-13 14:02:02 +0000

    valudation results summary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,8 +3,13 @@
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
+
 }
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+})
+
+$(document).ready(function () {
+  window.MOJFrontend.initAll()
 })
